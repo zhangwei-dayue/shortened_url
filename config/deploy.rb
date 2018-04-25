79bd535b03111c145aa87b22d9df241b4a456ecd
zhangwei <zhangwei@cinehello.com>
2018-04-25 14:46:42 +0800

    修改 deploy 文件

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,48 +1,3 @@
-sh "ssh-add"
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.10.1"
-
-set :application, "url_shortener"
-set :repo_url, "git@github.com:zhangwei-dayue/shortened_url.git"
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-set :deploy_to, "/home/url_shortener"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
-append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-
-set :passenger_restart_with_touch, true
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
@@ -58,7 +13,7 @@ require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
 
 set :application_name, 'url_shortener'
 set :domain, 'zhangwei@47.105.50.78'
-set :deploy_to, 'zhangwei/var/www/url_shortener'
+set :deploy_to, '/var/www/url_shortener'
 set :repository, 'https://github.com/zhangwei-dayue/shortened_url.git'
 set :branch, 'master'
 
