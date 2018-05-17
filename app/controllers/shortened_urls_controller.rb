a1a4d8c5bf475f3ba9e7c51f675b2c3b064032eb
zhangwei <zhangwei@cinehello.com>
2018-05-17 20:06:25 +0800

    增加输出 png 文件的接口

--- app/controllers/shortened_urls_controller.rb
+++ app/controllers/shortened_urls_controller.rb
@@ -37,6 +37,18 @@ class ShortenedUrlsController < ApplicationController
     end
   end
 
+  def send_png_file
+    @url = ShortenedUrl.find_by_original_url(url_params[:original_url])
+    render json: {message: '你请求的网站还未生成短网址，请先生成短网址再进行操作', success: false}, status: :unprocessable_entity and return if @url.blank?
+    prefix = "/public/system/dragonfly/production/"
+    data = open([Rails.root, prefix, @url.qr_code_uid].join).read
+    respond_to do |format|
+      format.png do
+        send_data data, type: 'image/png', disposition: 'inline'
+      end
+    end
+  end
+
   def shortened
     @url = ShortenedUrl.find_by_short_url(params[:short_url])
     host = request.host_with_port
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: redirect('http://cinehello.com/activities/#/')
   get "nobodyknows", to: "shortened_urls#index"
+  get :"/shortened_urls.png", to: "shortened_urls#send_png_file"
   get "/:short_url", to: "shortened_urls#show"
   get "shortened/:short_url", to: "shortened_urls#shortened", as: :shortened
   post :"/shortened_urls", to: "shortened_urls#create"
