e440bccd663a44e7187c4d3dcee8ed7fe7739167
zhangwei <zhangwei@cinehello.com>
2018-05-28 09:31:18 +0800

    修正数据库中的时区问题

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,10 @@ module UrlShortenerApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # 将数据库里的时间设置为和本地时间一致
     config.time_zone = "Beijing"
+    config.active_record.default_timezone = :local
+    config.active_record.time_zone_aware_attributes = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
